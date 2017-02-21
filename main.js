668736a0d4d34d3fa863b7a0ad68d13608017303
skiprox <seanmichaelscanlan@gmail.com>
2017-02-21 16:40:34 -0500

    Adding in canvas drawing

--- main.js
+++ main.js
@@ -11,50 +11,57 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+class Main {
+	constructor() {
+		this.mainWindow = null;
+		this.addListeners();
+	}
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+	createWindow() {
+		// Create the browser window.
+		this.mainWindow = new BrowserWindow({width: 1400, height: 900});
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+		// and load the index.html of the app.
+		this.mainWindow.loadURL(url.format({
+			pathname: path.join(__dirname, 'index.html'),
+			protocol: 'file:',
+			slashes: true
+		}));
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+		// Open the DevTools.
+		//this.mainWindow.webContents.openDevTools()
+		//// Emitted when the window is closed.
+		this.mainWindow.on('closed', function () {
+			// Dereference the window object, usually you would store windows
+			// in an array if your app supports multi windows, this is the time
+			// when you should delete the corresponding element.
+			this.mainWindow = null;
+		});
+	}
+
+	addListeners() {
+		// This method will be called when Electron has finished
+		// initialization and is ready to create browser windows.
+		// Some APIs can only be used after this event occurs.
+		app.on('ready', this.createWindow);
+
+		// Quit when all windows are closed.
+		app.on('window-all-closed', function () {
+			// On OS X it is common for applications and their menu bar
+			// to stay active until the user quits explicitly with Cmd + Q
+			if (process.platform !== 'darwin') {
+				app.quit();
+			}
+		});
+
+		app.on('activate', function () {
+			// On OS X it's common to re-create a window in the app when the
+			// dock icon is clicked and there are no other windows open.
+			if (this.mainWindow === null) {
+				this.createWindow();
+			}
+		});
+	}
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+new Main();
--- modules/lines.js
+++ modules/lines.js
@@ -1,10 +1,69 @@
 'use strict';
 
+const CONSTANTS = {
+	interval: 10,
+	duration: 2000,
+	width: 2
+};
+
+
+
 class Lines {
 	constructor(canvas, ctx) {
+		console.log('we load the lines');
 		this.canvas = canvas;
 		this.ctx = ctx;
-		console.log('hi mom', this.canvas, this.ctx);
+		this.width = null;
+		this.height = null;
+		this.interval = null;
+		this.counter = 0;
+		this.addBindings();
+		this.addListeners();
+		this.update();
+		this.makeDrawing();
+	}
+
+	addBindings() {
+		this.update = this.update.bind(this);
+		this.updateDrawing = this.updateDrawing.bind(this);
+	}
+
+	addListeners() {
+		window.addEventListener('resize', this.update);
+	}
+
+	update() {
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
+	}
+
+	makeDrawing() {
+		this.interval = window.setInterval(this.updateDrawing, CONSTANTS.interval);
+	}
+
+	stopDrawing() {
+		window.clearInterval(this.interval);
+	}
+
+	updateDrawing() {
+		this.draw(Math.floor(Math.random() * this.width), Math.floor(Math.random() * this.height));
+		this.counter++;
+		if (this.counter >= CONSTANTS.duration) {
+			this.stopDrawing();
+		}
+	}
+
+	draw(x, y) {
+		this.ctx.beginPath();
+		this.ctx.translate(this.width/2, this.height/2);
+		this.ctx.rotate(1.2*Math.PI/180);
+		this.ctx.translate(-this.width/2, -this.height/2);
+		this.ctx.rect(x, 0, CONSTANTS.width, this.height);
+		var rgbString1 = 'rgb(' + Math.floor((x/this.width)*255) + ', ' + Math.floor((y/this.height)*255) + ', 100)';
+		var rgbString2 = 'rgb(100, ' + Math.floor((x/this.width)*255) + ', ' + Math.floor((y/this.height)*255) + ')';
+		var rgbString3 = 'rgb(' + Math.floor((x/this.width)*255) + ', 100,' + Math.floor((y/this.height)*255) + ')';
+		this.ctx.fillStyle = rgbString1;
+		this.ctx.fill();
 	}
 }
 
