926902198617e6c31264126a8e42262364b29353
Sean Scanlan <sean.scanlan@kettlenyc.com>
2017-02-23 16:19:55 -0500

    Adding in static image examples

---
+++ modules/circle-image.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const CONSTANTS = {
+	radius: 300
+};
+
+const TakeScreenshot = require('./take-screenshot');
+
+class CircleImages {
+	constructor(canvas, ctx, imgSrc = 'img/corgi.png', x, y, radius, w, h) {
+		this.canvas = canvas;
+		this.ctx = ctx;
+		this.image = new Image();
+		this.image.src = imgSrc;
+		this.width = null;
+		this.height = null;
+		this.update();
+		this.x = x;
+		this.y = y;
+		this.radius = radius || CONSTANTS.radius;
+		this.w = w;
+		this.h = h;
+		this.beforeStart();
+		this.makeDrawing();
+	}
+
+	addBindings() {
+		this.update = this.update.bind(this);
+	}
+
+	update() {
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
+	}
+
+	beforeStart() {
+		this.ctx.fillStyle = '#ffffff';
+		this.ctx.fillRect(0, 0, this.width, this.height);
+	}
+
+	makeDrawing() {
+		let i = 20;
+		let x = 0;
+		let y = 0;
+		let radians = null;
+		this.image.onload = () => {
+			while (i--) {
+				radians = (i/20 * 360) * Math.PI/180;
+				x = this.x + this.radius * Math.cos(radians);
+				y = this.y + this.radius * Math.sin(radians);
+				this.draw(x, y, this.w, this.h);
+			}
+			this.takeScreenshot(1);
+		}
+	}
+
+	draw(x, y, w, h) {
+		this.ctx.beginPath();
+		this.ctx.drawImage(this.image, x - w/2, y - h/2, w, h);
+	}
+
+	takeScreenshot(index) {
+		new TakeScreenshot(index, 'screenshots/image-');
+	}
+}
+
+module.exports = CircleImages;
--- renderer.js
+++ renderer.js
@@ -3,6 +3,7 @@
 const Lines = require('./modules/lines');
 const Dots = require('./modules/dots');
 const Image = require('./modules/image');
+const CircleImage = require('./modules/circle-image');
 
 class Renderer {
 	constructor() {
@@ -13,10 +14,19 @@ class Renderer {
 		this.addBindings();
 		this.addListeners();
 		this.update();
-		new Lines(this.canvas, this.ctx, false);
+		//new Lines(this.canvas, this.ctx, false);
 		//new Dots(this.canvas, this.ctx);
-		new Image(this.canvas, this.ctx, true, 'img/corgi.png');
+		//new Image(this.canvas, this.ctx, true, 'img/corgi.png');
 		//new Image(this.canvas, this.ctx, false, 'img/zizek.png');
+		new CircleImage(this.canvas, this.ctx, 'img/corgi.png', 0, 0, 300, 50, 59);
+		new CircleImage(this.canvas, this.ctx, 'img/corgi.png', this.width, 0, 300, 50, 59);
+		new CircleImage(this.canvas, this.ctx, 'img/corgi.png', 0, this.height, 300, 50, 59);
+		new CircleImage(this.canvas, this.ctx, 'img/corgi.png', this.width, this.height, 300, 50, 59);
+		new CircleImage(this.canvas, this.ctx, 'img/tea.png', 260, 260, 300, 80, 80);
+		new CircleImage(this.canvas, this.ctx, 'img/tea.png', this.width - 260, 260, 300, 80, 80);
+		new CircleImage(this.canvas, this.ctx, 'img/tea.png', 260, this.height - 260, 300, 80, 80);
+		new CircleImage(this.canvas, this.ctx, 'img/tea.png', this.width - 260, this.height - 260, 300, 80, 80);
+		new CircleImage(this.canvas, this.ctx, 'img/corgi.png', this.width/2, this.height/2, 300, 100, 118);
 	}
 
 	addBindings() {
